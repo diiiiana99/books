29878f27656682796b78c0615372929a700010f0
Henry Miller <mail.henrym+git@gmail.com>
2022-02-14 11:56:56 -0500

    state working

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, {useState,useEffect} from 'react';
 // using axios because it is a simpler way of using CRUD, eliminates the r=>r.json() step
 import axios from "axios";
 import '../App.css';
-import Card from "./Card";
 
 // updates to state variables are being delayed by one action
 // ie: if you first search for 'javascript', it will return nothing
@@ -10,11 +9,11 @@ import Card from "./Card";
 
 ///TESTING API FUNCTIONALITY - not super important right now
 
-const apiKey='AIzaSyDQgG9PY_tH65Mss-EP1a8M_YQNZqORmys'
-// const apiKey= 'AIzaSyCu0GO52L8knIMQ7P_gmazBf_7wlngXqyc'
+const apiKey1 ='AIzaSyDQgG9PY_tH65Mss-EP1a8M_YQNZqORmys'
+const apiKey2='AIzaSyBOTcf1Js7o8SlmGyfA0bG7JBLrWJQ37R8'
 
 //search in title and author
-let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=atomic+inauthor:clear&printType=books&projection=lite&key=AIzaSyDQgG9PY_tH65Mss-EP1a8M_YQNZqORmys'
+let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=atomic+inauthor:clear&printType=books&key='+apiKey2
 
 //get singular result
 // uses volume ID 
@@ -22,30 +21,18 @@ let single = "https://www.googleapis.com/books/v1/volumes/XfFvDwAAQBAJ?key=AIzaS
 
 function App() {
   let [books,setBooks]= useState([])
-  let [results,setResults]=useState([])
-  const [booksToDisplay,setBTD]=useState([])
 
-  // let [booksToDisplay,setBooksTD]=useState([])
-
+  // initial book render
   useEffect(()=>{
     axios.get(booksUrl)
     .then(r=> {
-    //  { console.log(r.data.items)
-      setResults(r.data.items)
-      console.log(results)
-      if (results.length !==0){
-        setBTD(results.map((book)=>{
-          console.log(book)
-          return <Card book={book} />
-        //   return <img src={book.volumeInfo.imageLinks === undefined
-        //       ? ""
-        //       : `${book.volumeInfo.imageLinks.thumbnail}`
-        // } alt={book.title}/>
-        }))
-    }})
-
-  }, [])
-
+      console.log(r.data.items)
+      setBooks(r.data.items)
+    })
+  }
+, [])
+  
+  //search function
   const [search,setSearch]=useState('');
 
   function handleChange(event){
@@ -54,36 +41,23 @@ function App() {
 
   function handleSubmit(event){
     event.preventDefault();
-    let burl= `https://www.googleapis.com/books/v1/volumes?q=${search}&printType=books&key=${apiKey}&maxResults=40`
-    // console.log(burl)
+    let burl= `https://www.googleapis.com/books/v1/volumes?q=${search}&printType=books&key=${apiKey2}&maxResults=40`
+    console.log(burl)
     axios.get(burl)
     .then(r=> {
-    //  { console.log(r.data.items)
-      setResults(r.data.items)
-      console.log(results)
-      if (results.length !==0){
-        setBTD(results.map((book)=>{
-          console.log(book)
-          return <Card book={book} />
-        //   return <img src={book.volumeInfo.imageLinks === undefined
-        //       ? ""
-        //       : `${book.volumeInfo.imageLinks.thumbnail}`
-        // // } alt={book.title}/>
-        }))
-    }})
-    // fetch(burl)
-    // .then(r=>r.json())
-    // .then(items=>{
-    //   setResults(items)
-    //   console.log(results)
-    //   console.log(items)
-
-    // })
+      setBooks(r.data.items)
+      console.log(books)  
+  })
   }
 
-  // let booksToDisplay = books.items.map((book)=>{
-  //   return <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
-  // })
+  // update books to display 
+  // will not be staying in app component  
+  let booksToDisplay = books.map((book)=>{
+    return <img src={book.volumeInfo.imageLinks === undefined
+      ? ""
+      : `${book.volumeInfo.imageLinks.thumbnail}`
+      } alt={book.title}/>
+})
 
   return (
     <div className="App">
@@ -102,7 +76,6 @@ function App() {
               className='btn btn-danger'  
             >Search</button>
         </form>
-        {/* <Card/> */}
         {booksToDisplay}
       </div>
     </div>
