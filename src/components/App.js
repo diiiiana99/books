2715904fc0b1b6445c6bbcc99d6bcc97215270af
Henry Miller <mail.henrym+git@gmail.com>
2022-02-15 13:39:45 -0500

    best sellers functionality, apikey changes

--- src/components/App.js
+++ src/components/App.js
@@ -14,25 +14,35 @@ import { Route, Switch } from "react-router-dom";
 
 const apiKey1 ='AIzaSyDQgG9PY_tH65Mss-EP1a8M_YQNZqORmys'
 const apiKey2='AIzaSyBOTcf1Js7o8SlmGyfA0bG7JBLrWJQ37R8'
+const apiKey3='AIzaSyA8fo7zDXJrVGuia8Pml1RT8WIxmWA5Hzg'
+const apiKey4='AIzaSyA2Dg912lB4wLmPm0XaY--L-yc2PfdVG84'
+const apiKey5='AIzaSyDnjytUM4gOUWl7PpUTriSO3K8DuCBemiU'
+const apiKey6= 'AIzaSyA8fo7zDXJrVGuia8Pml1RT8WIxmWA5Hzg'
+const apiKey7= 'AIzaSyDVGpNSqYZdyylw2q3fRDfa2cVe2A7xDHU'
+const apiKey8 = 'AIzaSyB74bDSFkfuOu0WV_Z7iPIPUOAEiq2Mmbg'
+const apiKey='AIzaSyACwOvdEqnIZO3oG-IP35G2-XFB6EQqIts'
 
 //search in title and author
-let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=atomic+inauthor:clear&printType=books&key='+apiKey2
+let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=atomic+inauthor:clear&printType=books&key='+apiKey
 
-let zebras = 'https://www.googleapis.com/books/v1/volumes?q=zebras&printType=books&maxResults=25&key='+apiKey2
+let zebras = 'https://www.googleapis.com/books/v1/volumes?q=zebras&printType=books&maxResults=25&key='+apiKey
+
+let localZebras = 'http://localhost:3000/zebras'
 
 //get singular result
 // uses volume ID 
-let single = "https://www.googleapis.com/books/v1/volumes/XfFvDwAAQBAJ?key="+apiKey2
+let single = "https://www.googleapis.com/books/v1/volumes/XfFvDwAAQBAJ?key="+apiKey
 
 function App() {
   let [books,setBooks]= useState([])
 
   // initial book render
   useEffect(()=>{
-    axios.get(zebras)
+    axios.get(localZebras)
     .then(r=> {
-      setBooks(r.data.items)
+      setBooks(r.data)
     })
+    console.log(books)
   }, [])
 
   return (
@@ -40,12 +50,13 @@ function App() {
       <div className='container'>
         <NavBar 
           setBooks={setBooks}
-          apiKey2={apiKey2}
+          apiKey={apiKey}
         />
         <Switch>
           <Route path="/books">
             <BookList
               books={books}
+              apiKey={apiKey}
             />
           </Route>
           <Route exact path="/">
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,27 +1,80 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import BookCard from "./BookCard";
+import axios from "axios";
 
-function BookList( {books} ) {
+function BookList( {books, apiKey} ) {
       // update books to display 
     let filteredBooks = books.filter((book)=>{
         return book.volumeInfo.imageLinks !== undefined
     })
+
     let booksToDisplay = filteredBooks.map((book)=>{
         return (
             <div className='book-card' >
                 <img 
+                key={book.id}
                 src={book.volumeInfo.imageLinks === undefined? "": `${book.volumeInfo.imageLinks.thumbnail}`} 
                 alt={book.title}/>
             </div>
         )
-
         })
 
-    
+    let nytURL='https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=a8Y1LuFG60g1s9EqYAQ0ZgxEdIZIdnj3';
+
+    // use isbn number to get google api results and then display 
+
+    let [bestSellers,setBS]= useState([])
+
+    useEffect(()=>{
+      axios.get(nytURL)
+      .then(r=> {
+        setBS(r.data.results.books)
+      })
+    }, [])
+
+    let isbnList = bestSellers.map((book)=>{
+        // console.log(book)
+        return book.isbns[0]['isbn10']
+    })
+
+    let [topBooks,setTB] = useState([])
+    let nytbooks = [];
+
+    useEffect(()=>{
+        isbnList.map((book)=>{
+            // console.log(book.isbns[0]['isbn10'])
+            axios.get('https://www.googleapis.com/books/v1/volumes?q=+isbn:'+book+'&printType=books&key='+apiKey)
+            .then(r=> {
+                nytbooks.push(r.data.items)
+                setTB(nytbooks)
+            })
+        })
+    },[bestSellers])
+   
+    // let bestSellersToDisplay = topBooks.map((book)=>{
+    //         console.log(bestSellersToDisplay)
+    //         return (
+    //             <div className='book-card' >
+    //                 <img 
+    //                 src={book[0].volumeInfo.imageLinks.thumbnail} 
+    //                 alt={book.title}/>
+    //             </div>
+    //         )
+    //         })
+
+
     return(
-        <div className='book-carousel'> 
-                {booksToDisplay}
-        </div>    
+        <React.Fragment>
+            <div className='book-carousel'> 
+                    {booksToDisplay}
+            </div>
+            <div className='book-carousel'> 
+                    {/* {bestSellersToDisplay} */}
+            </div> 
+        </React.Fragment>
+
+         
+        
     );
 }
 
